414cf4a2b3551c813bad90a35e1d236c333c0dcc
RdEl00 <elaslireda35@gmail.com>
2022-09-08 20:27:44 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	/*string initialisation*/
-	char s[] = "Programming is like building a multilingual puzzle\n";
-	puts(s); /*writing the string to stdout*/
+	char s[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(s);
 	return(0);
 }
