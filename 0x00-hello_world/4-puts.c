50bb982e53a9803a969c242c40505968702162d2
RdEl00 <elaslireda35@gmail.com>
2022-09-08 20:23:56 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	//string initialisation
+	char s[] = "Programming is like building a multilingual puzzle\n";
+	puts(s); //writing the string to stdout
+	return(0);
+}
